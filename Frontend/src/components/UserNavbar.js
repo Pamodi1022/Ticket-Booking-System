05889e032c1cf17ce3d572333bb267e1ebb24bb2
Pamodi1022 <pamodipansiluni1022@gmail.com>
2025-03-01 20:23:14 +0530

    feat: Update UserNavbar component to correct logo path and enhance navigation links

--- Frontend/src/components/UserNavbar.js
+++ Frontend/src/components/UserNavbar.js
@@ -1,26 +1,30 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-import logo from './logo1.png';  // Make sure the path is correct
+import logo from '../assets/logo1.png';  // Make sure the path is correct
 
 export default function UserNavbar() {
   const [menuActive, setMenuActive] = useState(false);
-
+  
   const toggleMenu = () => {
     setMenuActive(!menuActive);
   };
-
+  
   return (
     <div className="navbar">
       <div className="logo-container">
-        <img src={logo} alt="Ticket Logo" className="logo-img" />  {/* Add the logo image */}
-        <h1 className="logo-text">Ticket.lk</h1>  {/* Logo text */}
+        <Link to="/">
+          <img src={logo} alt="Ticket Logo" className="logo-img" />
+        </Link>
+        <h1 className="logo-text">Ticket.lk</h1>
       </div>
       <button className="menu-toggle" onClick={toggleMenu}>
         ☰
       </button>
       <ul className={menuActive ? 'active' : ''}>
+        <li><Link to="/" onClick={toggleMenu}>Home</Link></li>
         <li><Link to="/TicketDisplay" onClick={toggleMenu}>Tickets</Link></li>
         <li><Link to="/LogDisplay" onClick={toggleMenu}>Ticket Log</Link></li>
+        <li><Link to="/config" onClick={toggleMenu}>Configuration</Link></li>
       </ul>
     </div>
   );
