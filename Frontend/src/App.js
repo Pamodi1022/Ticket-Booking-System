4385201e9dd9e77d348b8fe45fb534281c5cf10c
Pamodi1022 <pamodipansiluni1022@gmail.com>
2025-03-01 20:17:27 +0530

    feat: Update routing in App component to include Home and ViewDetail pages

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 
-import ConfigForm from './components/ConfigForm.js';
+import Home from './components/Home';
+import ConfigForm from './components/ConfigForm';
 import TicketsDisplay from './components/TicketsDisplay';
 import LogDisplay from './components/LogDisplay';
+import ViewDetail from './components/ViewDetail';
 
 
 function App() {
@@ -12,9 +14,12 @@ function App() {
     <Router>
       <div>
         <Routes>
-          <Route path="/" element={<ConfigForm />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/config" element={<ConfigForm />} />
           <Route path="/TicketDisplay" element={<TicketsDisplay />} />
           <Route path="/LogDisplay" element={<LogDisplay />} />
+          <Route path="/ViewDetail" element={<ViewDetail />} />
+          <Route path="*" element={<Navigate to="/" />} />
         </Routes>
       </div>
     </Router>
